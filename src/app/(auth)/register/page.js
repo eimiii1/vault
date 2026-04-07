7f8a98141e144d3b9bbe441a984fe49229713bea
eimiii1 <philipjohn1627@gmail.com>
2026-04-07 22:26:44 +0800

    ui: add a desktop screen

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -8,6 +8,8 @@ import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { HugeiconsIcon } from '@hugeicons/react'
 import { AlertCircleIcon } from '@hugeicons/core-free-icons'
 import Link from 'next/link'
+import Image from 'next/image'
+import { Separator } from '@/components/ui/separator'
 
 export default function Register() {
     const [name, setName] = useState('')
@@ -50,78 +52,91 @@ export default function Register() {
     }
 
     return (
-        <div
-            className='flex flex-col justify-start items-center p-5 h-screen w-screen'
-        >
-            <header className='flex flex-col justify-center items-center p-10 gap-2'>
-                <h1 className='font-semibold text-4xl w-full text-center'>Create your account</h1>
-                <p className='text-[#5f4bd2] text-sm font-semibold'>Join Ledger. Manage Smarter.</p>
-            </header>
+        <div className='flex p-5 h-screen w-screen'>
+            <div
+                className='flex flex-col justify-start lg:justify-center lg:p-18 items-center flex-1'
+            >
+                <header className='flex flex-col justify-center items-center p-10 gap-2'>
+                    <h1 className='font-semibold text-4xl w-full text-center'>Create your account</h1>
+                    <p className='text-[#5f4bd2] text-sm font-semibold'>Join Ledger. Manage Smarter.</p>
+                </header>
 
-            {error ? (
-                <Alert variant='destructive' className='max-w-md border-0'>
-                    <HugeiconsIcon icon={AlertCircleIcon} />
-                    <AlertTitle>Registration failed</AlertTitle>
-                    <AlertDescription>
-                        <span
-                            className='flex flex-col'
-                        >
-                            • {error}
-                        </span>
-                    </AlertDescription>
-                </Alert>
-            ) : null}
+                {error ? (
+                    <Alert variant='destructive' className='max-w-md border-0'>
+                        <HugeiconsIcon icon={AlertCircleIcon} />
+                        <AlertTitle>Registration failed</AlertTitle>
+                        <AlertDescription>
+                            <span
+                                className='flex flex-col'
+                            >
+                                • {error}
+                            </span>
+                        </AlertDescription>
+                    </Alert>
+                ) : null}
 
-            <div className='flex flex-col gap-4 w-screen p-10'>
-                <div className='flex flex-col gap-2'>
-                    <Label>Name</Label>
-                    <Input
-                        className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                        required
-                        type='text'
-                    />
+                <div className='flex flex-col gap-4 w-full p-4'>
+                    <div className='flex flex-col gap-2'>
+                        <Label>Name</Label>
+                        <Input
+                            className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                            placeholder='john.doe'
+                            required
+                            type='text'
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <Label>Email</Label>
+                        <Input
+                            className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
+                            value={emailAddress}
+                            onChange={e => setEmailAddress(e.target.value)}
+                            placeholder='john.doe@gmail.com'
+                            required
+                            type='email'
+                        />
+                    </div>
+                    <div className='flex flex-col gap-2'>
+                        <Label>Password</Label>
+                        <Input
+                            className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            placeholder='••••••••'
+                            required
+                            type='password'
+                        />
+                    </div>
                 </div>
-                <div className='flex flex-col gap-2'>
-                    <Label>Email</Label>
-                    <Input
-                        className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
-                        value={emailAddress}
-                        onChange={e => setEmailAddress(e.target.value)}
-                        required
-                        type='email'
-                    />
+                <div className='w-full text-center text-sm'>
+                    By signing up you are agreeing to our{' '}
+                    <span className='text-[#5f4bd2] font-semibold'>Terms & Conditions</span>
+                    {' '}and{' '}
+                    <span className='text-[#5f4bd2] font-semibold'>Privacy Policy</span>
                 </div>
-                <div className='flex flex-col gap-2'>
-                    <Label>Password</Label>
-                    <Input
-                        className='bg-transparent rounded-sm p-5 focus-visible:ring-[#5f4bd2] focus-visible:ring-[1.5px]'
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        required
-                        type='password'
-                    />
+                <div className='w-full p-4 pb-20'>
+                    <Button
+                        className='w-full p-6 font-bold rounded-sm'
+                        style={{ background: 'linear-gradient(to right, #4b3c9f, #5f4bd2)' }}
+                        type='button'
+                        onClick={handleRegister}
+                    >SIGN UP</Button>
                 </div>
+                <footer>
+                    <span className='font-medium opacity-60'>Already a member? </span> {' '}
+                    <Link href='/login' className='text-[#5f4bd2] font-bold'>Login</Link>
+                </footer>
             </div>
-            <div className='w-[60vw] text-center text-sm'>
-                By signing up you are agreeing to our{' '}
-                <span className='text-[#5f4bd2] font-semibold'>Terms & Conditions</span>
-                {' '}and{' '}
-                <span className='text-[#5f4bd2] font-semibold'>Privacy Policy</span>
+            <div className='hidden lg:flex flex-3 justify-center items-center relative'>
+                <Image 
+                src='/auth/ledger_auth_illustration.svg'
+                alt=''
+                fill
+                className='object-contain'
+                />
             </div>
-            <div className='w-screen p-10 pb-20'>
-                <Button
-                    className='w-full p-6 font-bold rounded-sm'
-                    style={{ background: 'linear-gradient(to right, #4b3c9f, #5f4bd2)' }}
-                    type='button'
-                    onClick={handleRegister}
-                >SIGN UP</Button>
-            </div>
-            <footer>
-                <span className='font-medium opacity-60'>Already a member? </span> {' '}
-                <Link href='/login' className='text-[#5f4bd2] font-bold'>Login</Link>
-            </footer>
         </div>
     )
 }
